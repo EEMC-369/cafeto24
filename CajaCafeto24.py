42f3367cad961cdf252ba5020381d7f8965c03d1
EEMC-369 <em386@example.com>
2026-07-23 01:31:02 -0500

    Enable multiple barcodes separated by commas or spaces for a single product v3.16.0

--- CajaCafeto24.py
+++ CajaCafeto24.py
@@ -54,7 +54,7 @@ else:
     RESOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
     USER_DATA_DIR = os.path.dirname(os.path.abspath(__file__))
 
-VERSION = "3.15.0"
+VERSION = "3.16.0"
 
 app = Flask(
     __name__,
--- instalador.iss
+++ instalador.iss
@@ -3,7 +3,7 @@
 
 [Setup]
 AppName=Cafeto24
-AppVersion=3.15.0
+AppVersion=3.16.0
 AppPublisher=Cafeto24
 DefaultDirName={autopf}\Cafeto24
 DefaultGroupName=Cafeto24
